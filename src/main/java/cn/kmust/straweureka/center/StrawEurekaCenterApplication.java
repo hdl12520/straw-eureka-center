073e8b6dceb3da8b4633320b9eee1681bd1f702b
hdl12520 <1750094549@qq.com>
2020-08-07 10:26:57 +0800

    首次提交项目

---
+++ src/main/java/cn/kmust/straweureka/center/StrawEurekaCenterApplication.java
@@ -0,0 +1,13 @@
+package cn.kmust.straweureka.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrawEurekaCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrawEurekaCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/kmust/straweureka/center/StrawEurekaCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.kmust.straweureka.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrawEurekaCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
